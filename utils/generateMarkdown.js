09aa0fe030cfd3dd0fb8d112a0b04a01638d4cfa
Andrew Olson <frrrkin@gmail.com>
2020-12-06 10:43:11 -0800

    finished the template and questions - only filling in data for more licenses remains

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,8 +3,39 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
-  
-  `;
+
+![License](${data.license.badge})
+
+## Table Of Contents
+1.  [Description](#description)
+2.  [Installation](#installation)
+3.  [Usage](#usage)
+4.  [License](#license)
+5.  [Contributing](#contributing)
+6.  [Tests](#tests)
+7.  [Questions](#questions)
+
+## Description
+${data.description} 
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+<b>${data.license.name}</b>
+
+${data.license.summary}
+
+Read the full license here: ${data.license.linkToFull}
+## Contributing
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+github me at ${data.githubUsername}
+
+email me at ${data.email}
+`;
   }
   
 module.exports = generateMarkdown;
